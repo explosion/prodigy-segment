b5c45e97e3c72319ec010026b2129f4ce2dd7796
Vincent <vincent@calmcode.io>
2023-12-12 12:40:21 +0100

    adding some tests

---
+++ tests/test_basics.py
@@ -0,0 +1,6 @@
+from prodigy_segment import get_base64_string
+
+
+def test_get_base64_string():
+    example = {"image": "data:image/jpeg;base64,/9j/4AAQSkZJRgABAgAAZABkAAD"}
+    assert get_base64_string(example) == "/9j/4AAQSkZJRgABAgAAZABkAAD"
